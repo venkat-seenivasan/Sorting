e0fe32f1d342bd25b95da7cdfb01705b8a5d6c9e
venkat-seenivasan <venkateshsrinivasan2003@gmail.com>
2025-07-22 10:37:11 +0530

    Selection sort

---
+++ Sort_Selection.java
@@ -0,0 +1,26 @@
+package Sorting;
+
+public class Sort_Selection {
+    public static void main(String[] args) {
+        int arr[]={6,5,2,7,3,8};
+        int j;
+        for(int i=0;i<arr.length;i++){
+            int min=i;
+            for(j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                        min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+            
+
+           
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+}
