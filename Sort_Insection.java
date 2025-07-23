ed6971954599d9575bf7b028bc7ce25d43616727
venkat-seenivasan <venkateshsrinivasan2003@gmail.com>
2025-07-23 09:38:41 +0530

    Insertion sort

--- Sort_Insection.java
+++ Sort_Insection.java
@@ -1,6 +1,6 @@
 public class Sort_Insection {
     public static void main(String[] args) {
-         int arr[]={6,12,3,2,10,1,5,4};
+         int arr[]={6,12,3,7,2,10,1,5,4};
         for(int i=1;i<arr.length;i++){
             int key=arr[i];
             int j=i-1;
